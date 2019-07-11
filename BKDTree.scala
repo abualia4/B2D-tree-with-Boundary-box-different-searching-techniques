72cc85f15c337e7ca84fe83a702be9c47096ed46
Ahmed Alia <52276877+abualia4@users.noreply.github.com>
2019-07-11 08:31:29 +0200

    Update BKDTree.scala

--- BKDTree.scala
+++ BKDTree.scala
@@ -11,6 +11,18 @@
  class BKDTree{
     var current:Node=null
     /**
+     * To check if the two points are the same ot no.
+     * @param first is the first point
+     * @param seconf is the second point
+     * @return Node
+     */
+     def same(first: Point, second:Point):Boolean={
+        for(i<-0 to 1)
+         if(first.getPoint()(i)!=second.getPoint()(i))
+           return false
+        return true
+    }
+    /**
      * building the B2DT
      * Determining the boundary box for each point
      * @param current is the current node starting from  the root of B2DT
@@ -19,11 +31,9 @@
      * @return Node
      */
     def insertBKD(current:Node, value:Point, depth: Int ):Node ={
-         
-         if(current==null)
-         return null
-         
-    
+       
+        if(current==null||same(current.point,value))
+          return null
         var cd:Int=depth%2
         var newPoint=value.getPoint()
         var currentPoint=current.point.getPoint()
@@ -31,13 +41,13 @@
         /**
          * For X dimension/plane
          */
+          
         if(cd==0){ 
            if(newPoint(cd)<currentPoint(cd)){
              if(current.left!=null)
                  insertBKD(current.left,value,depth+1) 
               else{
                   current.left=new Node(value)
-                   
                   current.left.parent=current
                   current.left.rect=new Rectangle(current.rect.xMin(),current.rect.yMin(),currentPoint(0),current.rect.yMax())
                    
@@ -53,7 +63,6 @@
                     current.right.parent=current
                      
                     current.right.rect=new Rectangle(currentPoint(0),current.rect.yMin(),current.rect.xMax(),current.rect.yMax())
- 
 
                 }
 
@@ -112,7 +121,7 @@
          if(current==null)
          {
              current=new Node(value)
-             current.rect=new Rectangle(0, 0, 9, 10);
+             current.rect=new Rectangle(0, 0, 10000000, 10000000);
              
          }
          else
@@ -125,7 +134,7 @@
      * @param depth the current depth/level of the B2DT
      */
     def insertList (points: List[Point], depth:Int ):Unit  ={
-    
+        
          if( points!=null)
          {       
              
